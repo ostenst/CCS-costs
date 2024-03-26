4d7b81c30f2b154c379e50a5ab4de15820737499
Oscar Stenström <93720714+ostenst@users.noreply.github.com>
2024-03-26 15:46:36 +0100

    Cleaning done, ready to do costing

--- controller.py
+++ controller.py
@@ -5,132 +5,76 @@ Later, the Controller() will ask the Model() to run many times given plant_data.
 import pandas as pd
 import matplotlib.pyplot as plt
 from functions import *
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.linear_model import LinearRegression
 
-# DEFINE MY INPUT DATA
-W2E_data = pd.read_csv("W2E.csv", sep=";", decimal=',') 
+# DEFINE MY INPUT DATA AND REGRESSIONS OF ASPEN DATA
 plant_data = pd.read_csv("plant_data.csv", sep=";", decimal=',')
 print(plant_data.head())
 
+W2E_data = pd.read_csv("W2E.csv", sep=";", decimal=',')
+X = W2E_data[['CO2%', 'Flow']]
+y = W2E_data.drop(columns=['CO2%', 'Flow'])
+
+W2E_regression = MultiOutputRegressor(LinearRegression())
+W2E_regression.fit(X, y)
+
 for index, row in plant_data.iterrows():
-    # INITIALIZE AND EVALUATE THE CHP
-    chp = CHP(
-        Name=row["Plant Name"],
-        Fuel=row["Fuel (W=waste, B=biomass)"],
+    
+    # INITIALIZE CHP
+    CHP = CHP_plant(
+        name=row["Plant Name"],
+        fuel=row["Fuel (W=waste, B=biomass)"],
         Qdh=float(row["Heat output (MWheat)"]),
         P=float(row["Electric output (MWe)"]),
         Qfgc=float(row["Existing FGC heat output (MWheat)"]),
+        ybirth=float(row["Year of commissioning"]),
         Tsteam=float(row["Live steam temperature (degC)"]),
         psteam=float(row["Live steam pressure (bar)"])
     )
-    chp.estimate_performance()
-    chp.print_info()
-    MEA = MEA_plant(chp) #should be f(volumeflow,%CO2)... maybe like this: MEA_plant(host=chp, constr_year, currency, discount, lifetime)
-    MEA.estimate_size(W2E_data)
-    print(MEA.data["Qdry"])
+    
+    # ESTIMATE ENERGY BALANCE AND FLUE GASES                                                   
+    CHP.estimate_performance()                                                                      # <--- BEGIN RDM EFTER THIS ESTIMATION? YES!
+    
+    if CHP.fuel == "B":
+        CHP.fCO2 = 0.16
+    if CHP.fuel == "W":
+        CHP.fCO2 = 0.11
+    
+    CHP.mCO2 = CHP.Qfuel * 0.355 #[tCO2/h]
+    CHP.Vfg = 2000000/110*CHP.mCO2/(CHP.fCO2/0.04) #[m3/h]
+    CHP.print_info()
 
-    # RDM MODEL SHOULD MAYBE START HERE? No... or well it should be before we assign discountrates etc. But maybe after extracting Aspen_data!
-    Plost, Qlost, reboiler_steam = chp.energy_penalty(MEA)
-    chp.print_info()
+    # ESTIMATE SIZE OF MEA PLANT AND THE ENERGY PENALTY
+    MEA = MEA_plant(CHP) #should be f(volumeflow,%CO2)... maybe like this: MEA_plant(host=chp, constr_year, currency, discount, lifetime)
 
-    chp.plot_plant()
-    chp.plot_plant(capture_states=reboiler_steam)
+    if CHP.fuel == "W":
+        MEA.estimate_size(W2E_regression, W2E_data)
+    if CHP.fuel == "B":
+        print("Aspen data not available for bio-chip fired")
+
+    dTreb = 10
+    Plost, Qlost, reboiler_steam = CHP.energy_penalty(MEA, dTreb)
+    CHP.print_info()
+
+    CHP.plot_plant()
+    CHP.plot_plant(capture_states=reboiler_steam)
 
     # HEAT INTEGRATION WORK BELOW
-    # considered_streams = ['wash', 'toabs', 'strip', 'lean', 'int2', 'int1', 'dcc', 'dhx', 'dry', 'dum', 'rcond', 'rint', 'preliq']
-    # considered_streams = ['wash', 'toabs', 'strip', 'lean', 'int2', 'int1', 'dhx', 'dry', 'rcond', 'rint', 'preliq']
-    considered_streams = ['wash', 'strip', 'lean', 'int2', 'int1', 'dhx', 'dry', 'rcond', 'rint', 'preliq'] # Add 'dcc' (not 'toabs'?) when DCC HEX is needed
+    if CHP.fuel == "W" or "B": # Arbitrary before industrial cases are added
+        consider_dcc = False
 
-    stream_data = MEA.identify_streams(considered_streams)
+    stream_data = MEA.identify_streams(consider_dcc)
+    composite_curve = MEA.merge_heat(stream_data)
     MEA.plot_streams(stream_data)
-
-    temperature_ranges = MEA.find_ranges(stream_data)
-    composite_curve = MEA.merge_heat(temperature_ranges, stream_data)
 
     Tsupp = 86
     Tlow = 38
-    dTmin = 7
+    MEA.dTmin = 7
 
-    Qsupp, Qlow, Qpinch, Tpinch = MEA.available_heat2(composite_curve, Tsupp, Tlow, dTmin=dTmin)
-    MEA.plot_hexchange(Qsupp, Qlow, Qpinch, Tpinch, dTmin, Tlow, Tsupp) #TODO: Move some values to the self. of CHP or MEA
-    Qrecovered = (Qpinch-Qsupp) + (Qlow-Qpinch)
-    Qcw = MEA.composite_curve[-1][0]-Qlow
-    chp.Qdh += round(Qrecovered/1000)
-    chp.print_info()
+    Qrecovered = MEA.available_heat(composite_curve, Tsupp, Tlow)
+    CHP.Qdh += round(Qrecovered/1000)
+    CHP.print_info()
+    MEA.plot_hexchange()
 
 plt.show()
-
-
-
-# # But what is the cost of this heat exchange? Find areas!
-# U = 1 #kW/m2K, (Deng compr study)
-# Alow, Ahigh, Acw = exchanger_areas(Qhighgrade, Qlowgrade, Qcw, U, dTmin, Tmax, Tsupp, Tlow, Tend)
-# print("I believe in these areas, but they are dissimilar from Aspen: ", Ahigh, Alow, Acw)
-
-# # Integrate the HEXs
-# Qdh, mcoolingwater = chp.heat_integrate(Qhighgrade, Qlowgrade, Qcw)
-# print(Qdh, mcoolingwater)
-# chp.print_info()
-
-
-
-# Aspen_data = MEA.linearReg(Aspen_data) #This is ok and can happen, but some things (e.g. Acool1) should not be used
-# ebalance = MEA.Ebalance(Aspen_data,chp)
-
-# direct_costs = []
-# for equipment in MEA.equipment_list:
-#     direct_cost = MEA.direct_cost(equipment)
-#     direct_costs.append(direct_cost)
-
-
-# plt.figure(figsize=(10, 6))
-# plt.bar(MEA.equipment_list, direct_costs, color='blue')
-# plt.xlabel('Equipment Names')
-# plt.ylabel('Direct Costs [kEUR]')
-# plt.title('Direct Costs of Equipment Items')
-# plt.xticks(rotation=45, ha='right')  
-# plt.tight_layout()
-# # plt.show()
-
-# # Non-EnergyCosts:
-# TDC = sum(direct_costs)
-# print(" ")
-# print("TotalDirectCost is", TDC)
-# print("TDC specific annualized is", MEA.specific_annualized(TDC) )
-# TCR, aCAPEX, cost_H2O, cost_MEA, cost_maintenance, cost_labor = MEA.NOAK_escalation(TDC)
-
-# # Rough estimate of EnergyCost (site-TEA):
-# steam_price = 28.4          #EUR/MWh
-# coolingwater_price = 0.02   #EUR/t
-# elec_price = 60             #EUR/MWh
-
-# Qreb = MEA.data["Q_REBOIL"].values[0] #kW
-# Qcool = 0                             #kW
-# for cooler in ['B4', 'COOL1', 'COOL2', 'COOL3', 'DCCHX', 'DRYCOOL', 'DUMCOOL']:
-#     key = 'Q_' + cooler
-#     Qcool += MEA.data[key].values[0] 
-# Welc = 0                                #kW
-# for pump in ['DCCPUMP', 'PUMP', 'B311']:
-#     key = 'W_' + pump
-#     Welc += MEA.data[key].values[0]
-
-# cost_steam = Qreb/1000 * MEA.duration * steam_price/1000 #kEUR/a
-# mcool = -Qcool/(4.18*15) #kg/s assuming cp=4.18kJ/kg and dT=15C)
-# cost_coolingwater = mcool/1000 * 3600*MEA.duration * coolingwater_price/1000 #kEUR/a
-# cost_elec = Welc/1000 * MEA.duration * elec_price/1000 #kEUR/a
-# print(cost_steam, cost_coolingwater, cost_elec)
-# print("Promising, because: CostSteam dominates, and is ~= 2*aCAPEX. Also CostWelc is about ~= CostSteam/10 (not counting Compr&Lique), and CostMaintenance is < aCAPEX/2. These general patterns are consistent with Ali, 2019. But MEAmakeup is weirdly high!")
-
-# variable_names = ['aCAPEX', 'cost_elec', 'cost_coolingwater', 'cost_steam', 'cost_MEA', 'cost_maintenance', 'cost_labor', 'cost_H2O']
-# variable_costs = [aCAPEX, cost_elec, cost_coolingwater, cost_steam, cost_MEA, cost_maintenance, cost_labor, cost_H2O]
-
-# # Plotting the bar chart
-# plt.figure(figsize=(10, 6))
-# plt.bar(variable_names, variable_costs, color='green')
-# plt.xlabel('Cost Variables')
-# plt.ylabel('Cost (kEUR/a)')
-# plt.title('Costs of Different Variables')
-# plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
-# plt.tight_layout()
-
-# # Show the plot
-# plt.show()
--- functions.py
+++ functions.py
@@ -39,27 +39,25 @@ class State:
     def __str__(self):
         return self.Name
         # return f"s = {round(self.s,1)} | T = {round(self.T)} | h = {round(self.h)}"  
+    
     def plot(self, pressure=False):
         plt.scatter(self.s, self.T, label=str(self), marker='x', color='r')
         plt.annotate( str(self)+': '+str(round(self.p,2))+' bar' if pressure else str(self), (self.s, self.T), textcoords="offset points", xytext=(5,5))
 
-class CHP:
-    def __init__(self, Name, Fuel=None, Qdh=0, P=0, Qfgc=0, Tsteam=0, psteam=0, Qfuel=0, Vfg=0, mCO2=0):
-        self.name = Name
-        self.fuel = Fuel
+class CHP_plant:
+    def __init__(self, name, fuel=None, Qdh=0, P=0, Qfgc=0, ybirth=0, Tsteam=0, psteam=0, Qfuel=0, Vfg=0, mCO2=0):
+        self.name = name
+        self.fuel = fuel
         self.Qdh = Qdh
         self.P = P
         self.Qfgc = Qfgc
+        self.ybirth = ybirth
         self.Tsteam = Tsteam
         self.psteam = psteam
         self.Qfuel = Qfuel
         self.Vfg = Vfg
+        self.fCO2 = 0
         self.mCO2 = mCO2
-
-        if Fuel == "B":
-            self.fCO2 = 0.16
-        elif Fuel == "W":
-            self.fCO2 = 0.11
         self.states = None
 
     def print_info(self):
@@ -69,9 +67,7 @@ class CHP:
         print(table_format.format(self.name, round(self.P), round(self.Qdh+self.Qfgc), round(self.Qfuel), self.fuel, round(self.Vfg)))
 
     def estimate_performance(self, plotting=False):
-        # ENERGY BALANCE
         Ptarget = self.P
-        print(Ptarget)
         Qdh = self.Qdh
         A = State("A", self.psteam, self.Tsteam)
 
@@ -89,24 +85,20 @@ class CHP:
             i += 1
         if i == max_iterations:
             Pestimated = None
-            msteam = 0
+
         Qfuel = msteam*(A.h-C.h)
         self.Qfuel = Qfuel
-        self.P = Pestimated             # TODO: Apply uncertainty% to P/Q?
+        self.P = Pestimated
         D = State("D", self.psteam, satL=True)
-
-        # FLUEGASES #TODO: calculate based on johanna+thunman
-        self.mCO2 = Qfuel * 0.355 #[tCO2/h]
-        self.Vfg = 2000000/110*self.mCO2/(self.fCO2/0.04) #[m3/h]
-
         self.states = A,B,C,D
+
         if plotting == True:
             self.plot_plant(A,B,C,D)
 
-        if msteam is not None and Pestimated is not None and Qfuel > 0 and pcond_guess > 0 and self.mCO2 > 0 and self.Vfg > 0:
+        if msteam is not None and Pestimated is not None and Qfuel > 0 and pcond_guess > 0:
             return
         else:
-            raise ValueError("One or more of the variables (msteam, Pestimated, Qfuel, pcond_guess, mCO2, Vfg) is not positive.")
+            raise ValueError("One or more of the variables (msteam, Pestimated, Qfuel, pcond_guess) is not positive.")
 
     def plot_plant(self, capture_states=None, show=False):
         A,B,C,D = self.states
@@ -153,10 +145,10 @@ class CHP:
             plt.show()
         return
     
-    def energy_penalty(self, MEA):
+    def energy_penalty(self, MEA, dTreb):
         A,B,C,D = self.states
         mtot = self.Qfuel*1000 / (A.h-C.h)
-        TCCS = MEA.get("Treb") + 10 #Assuming dTmin = 10 in reboiler MEA.get
+        TCCS = MEA.get("Treb") + dTreb
         pCCS = steamTable.psat_t(TCCS)
         Ta = steamTable.t_ps(pCCS,A.s)
 
@@ -179,44 +171,45 @@ class CHP:
 
         reboiler_steam = [a,d]
         return Plost, Qlost, reboiler_steam
-    
-    def heat_integrate(self, Qhighgrade, Qlowgrade, Qcw):
-        self.Qdh += (Qhighgrade + Qlowgrade)/1000
-        mcoolingwater = Qcw/(4.18*(20-15)) # Assuming cw properties, TODO: mcoolingwater is very high!? But the heat capacity Qcw seems reasonable.
-        return self.Qdh, mcoolingwater
 
 class MEA_plant:
     def __init__(self, host_plant, construction_year=2024, currency_factor=0, discount=0.08, lifetime=25):
         self.host = host_plant
         self.data = None
-        self.construction_year = construction_year
-        self.currency_factor = currency_factor
-        self.discount = discount
-        self.lifetime = lifetime
         self.composite_curve = None
+        self.dTmin = None
+        self.QTdict = None
 
     def get(self, parameter):
         return self.data[parameter].values[0]
 
-    def estimate_size(self, Aspen_data):
-        df = Aspen_data #TODO: Move this outside of RDM loop?
-        X = df[['CO2%', 'Flow']]
-        y = df.drop(columns=['CO2%', 'Flow'])
+    def estimate_size(self, model, Aspen_data):
+        # df = Aspen_data #TODO: Move this outside of RDM loop?
+        # X = df[['CO2%', 'Flow']]
+        # y = df.drop(columns=['CO2%', 'Flow'])
 
-        model = MultiOutputRegressor(LinearRegression())
-        model.fit(X, y)
+        # model = MultiOutputRegressor(LinearRegression())
+        # model.fit(X, y)
+
+        y = Aspen_data.drop(columns=['CO2%', 'Flow']) # Need to keep this, to be able to name the columns of predicted_df
+    
         print("Estimated flue gas volume: ", self.host.Vfg, " [m3/h], or ", self.host.Vfg/3600*0.8, " [kg/s]" )
         new_input = pd.DataFrame({'CO2%': [self.host.fCO2*100], 'Flow': [self.host.Vfg/3600*0.8]})  # Fraction of CO2=>percentage, and massflow [kg/s], of flue gases
         predicted_y = model.predict(new_input)
         predicted_df = pd.DataFrame(predicted_y, columns=y.columns)
+
         # print("MEA plant is this big: ")
         # print(predicted_df.head())
         self.data = predicted_df
         return 
 
-    def identify_streams(self, stream_indicies):
+    def identify_streams(self, consider_dcc):
+        considered_streams = ['wash', 'strip', 'lean', 'int2', 'int1', 'dhx', 'dry', 'rcond', 'rint', 'preliq'] # For CHPs
+        if consider_dcc:
+            considered_streams.append('dcc') # For industrial cases with hot flue gases that first need to be cooled before entering the amine absorber
+
         stream_data = {}
-        for component in stream_indicies:
+        for component in considered_streams:
             stream_data[component] = {
                 'Q': -self.get(f"Q{component}"),
                 'Tin': self.get(f"Tin{component}")-273.15,
@@ -229,7 +222,7 @@ class MEA_plant:
         for component, data in stream_data.items():
             temperatures.extend([data['Tin'], data['Tout']])
 
-        unique_temperatures = list(dict.fromkeys(temperatures))  # Remove duplicates by converting to dictionary keys and back to list
+        unique_temperatures = list(dict.fromkeys(temperatures)) 
         unique_temperatures.sort(reverse=True)
 
         temperature_ranges = []
@@ -239,8 +232,9 @@ class MEA_plant:
 
         return temperature_ranges
 
-    def merge_heat(self, temperature_ranges, stream_data):
-        # Should also return the actual composite curve points!
+    def merge_heat(self, stream_data):
+        temperature_ranges = self.find_ranges(stream_data)
+
         composite_curve = [[0, temperature_ranges[0][1]]] # First data point has 0 heat and the highest temperature
         Qranges = []
         for temperature_range in temperature_ranges:
@@ -259,99 +253,10 @@ class MEA_plant:
             composite_curve.append([sum(Qranges), temperature_range[0]])
         self.composite_curve = composite_curve
         return composite_curve
-
-    def plot_streams(self, stream_data, show=False):
-        plt.figure(figsize=(10, 8))
-        num_streams = len(stream_data)
-        colormap = plt.cm.get_cmap('Paired', num_streams)  # Using the Tab20B colormap
-
-        for i, (component, data) in enumerate(stream_data.items()):
-            Q = data['Q']
-            TIN = data['Tin']
-            TOUT = data['Tout']
-
-            # Assigning a unique color to each component based on Tab20B colormap
-            color = colormap(i)
-
-            plt.plot([0, Q], [TIN, TOUT], marker='o', color=color, label=f"Q{component}")
-
-        # Adding labels and title
-        plt.xlabel('Q [kW]')
-        plt.ylabel('Temperature [C]')
-        plt.title('Streams To Cool')
-        plt.legend()
-        plt.grid(True)
-        if show:
-            plt.show()
-
-    def plot_composite(self, temperature_ranges, Qranges, Tmax, Tsupp, Thigh, Tlow, dTmin=10, show=False):
-        plt.figure(figsize=(10, 8))
-        Tmax = Tmax - dTmin
-        current_q = 0
-        Qinteresting = []
-        for i, temp_range in enumerate(temperature_ranges):
-            next_q = current_q + Qranges[i]
-
-            for Tinteresting in [Tmax, Tsupp, Thigh, Tlow]: # I look for all temps in each range
-                if temp_range[0]-dTmin <= Tinteresting <= temp_range[1]-dTmin:
-                    q_value = current_q + (Tinteresting - (temp_range[1]-dTmin))*(next_q - current_q)/(temp_range[0] - temp_range[1])
-                    Qinteresting.append(q_value)
-
-            labels = ["Treal","Tshifted"] if i == 0 else [None, None]  
-            plt.plot([current_q, next_q], [temp_range[1], temp_range[0]], marker='o', color='#FF0000', label=labels[0])
-            plt.plot([current_q, next_q], [temp_range[1]-dTmin, temp_range[0]-dTmin], color='#FA8072', marker='o', label=labels[1])
-            current_q = next_q
-
-        # Plot heat sinks
-        plt.plot([0, self.get("Qreb")], [self.get("Treb"), self.get("Treb")], marker='*', color='#a100ba', label='Qreboiler')
-        plt.plot([Qinteresting[0], Qinteresting[2]], [Tsupp, Thigh], marker='o', color='#0000FF', label='DH high-grade')
-        plt.plot([Qinteresting[2], Qinteresting[3]], [Thigh, Tlow], marker='o', color='#069AF3', label='DH low-grade')
-        plt.plot([Qinteresting[3], current_q], [20, 15], marker='o', color='#0000FF', label='Cooling water') # NOTE: hard-coded CW temps.
-
-        # Annotate interesting info
-        Q_highgrade = Qinteresting[2]-Qinteresting[0] # dT are given by our assumptions! Composite curve has (120+10)C->(61+10)C , DH has 61C->86C
-        Q_lowgrade  = Qinteresting[3]-Qinteresting[2]
-        Q_cw = current_q - Qinteresting[3]
-        plt.text(26000, 70, f'dTmin={dTmin} C', color='black', fontsize=12, ha='center', va='center')
-        plt.text(26000, 115, f'Qreb={round(self.get("Qreb")/1000)} MW', color='#a100ba', fontsize=12, ha='center', va='center')       
-        plt.text(5000, 70, f'Qhighgrade={round(Q_highgrade/1000)} MW', color='#0000FF', fontsize=12, ha='center', va='center')
-        plt.text(5000, 52, f'Qlowgrade={round(Q_lowgrade/1000)} MW', color='#069AF3', fontsize=12, ha='center', va='center')
-        plt.text(10000, 20, f'Qcoolingwater={round(Q_cw/1000)} MW', color='#0000FF', fontsize=12, ha='center', va='center')
-
-        # Adding labels and title
-        plt.xlabel('Q [kW]')
-        plt.ylabel('Temperature [C]')
-        plt.title('Hot Composite Curve vs Cold Utilities')
-        plt.legend()
-        plt.grid(True)
-        if show:
-            plt.show()
-
-    def available_heat(self, temperature_ranges, Qranges, Tmax, Tsupp, Thigh, Tlow, dTmin=10):
-        # Looks for the real DH temperatures, at the shifted (dTmin) composite curve
-        # NOTE: this assumes heat exchange is possible between Tmax and Thigh, ensure this is true.
-        Tmax = Tmax - dTmin # This temp. belongs to the composite curve and should be shifted. Should be ok now!
-        current_q = 0
-        Qinteresting = []
-        for i, temp_range in enumerate(temperature_ranges): #IM IN ONE RANGE
-            
-            next_q = current_q + Qranges[i]  
-            for Tinteresting in [Tmax, Tsupp, Thigh, Tlow]: # I look for all temps in each range
-                if temp_range[0]-dTmin <= Tinteresting <= temp_range[1]-dTmin:
-                    q_value = current_q + (Tinteresting - (temp_range[1]-dTmin))*(next_q - current_q)/(temp_range[0] - temp_range[1])
-                    Qinteresting.append(q_value)
-            current_q = next_q
-        Tend = temp_range[0] 
-
-        Q_highgrade = Qinteresting[2]-Qinteresting[0] # dT are given by our assumptions! Composite curve has (120+10)C->(61+10)C , DH has 61C->86C
-        Q_lowgrade  = Qinteresting[3]-Qinteresting[2]
-        Q_cw = current_q - Qinteresting[3]
-
-        return Q_highgrade, Q_lowgrade, Q_cw, Tend
     
-    def available_heat2(self, composite_curve, Tsupp, Tlow, dTmin=10):
+    def available_heat(self, composite_curve, Tsupp, Tlow):
 
-        shifted_curve = [[point[0], point[1] - dTmin] for point in composite_curve]
+        shifted_curve = [[point[0], point[1] - self.dTmin] for point in composite_curve]
         curve = shifted_curve
         # Calculate the distances of each point from the line formed by connecting the endpoints. Find the elbow point (point of maximum curvature).
         def distance(p1, p2, p):
@@ -386,16 +291,49 @@ class MEA_plant:
         Qlow = linear_interpolation(curve, Tlow)
         Qpinch, Tpinch = curve[max_curvature_index][0], curve[max_curvature_index][1]
 
-        return Qsupp, Qlow, Qpinch, Tpinch
+        self.QTdict = {
+            "supp": [Qsupp, Tsupp],
+            "low": [Qlow, Tlow],
+            "pinch": [Qpinch, Tpinch]
+        }
+
+        Qrecovered = (Qpinch-Qsupp) + (Qlow-Qpinch)
+        return Qrecovered
     
-    def plot_hexchange(self, Qsupp, Qlow, Qpinch, Tpinch, dTmin, Tlow, Tsupp, show=False): #TODO: Move temperatures to the CHP or the MEA class!
+    def plot_streams(self, stream_data, show=False):
+        plt.figure(figsize=(10, 8))
+        num_streams = len(stream_data)
+        colormap = plt.cm.get_cmap('Paired', num_streams)  # Using the Tab20B colormap
+
+        for i, (component, data) in enumerate(stream_data.items()):
+            Q = data['Q']
+            TIN = data['Tin']
+            TOUT = data['Tout']
+
+            # Assigning a unique color to each component based on Tab20B colormap
+            color = colormap(i)
+
+            plt.plot([0, Q], [TIN, TOUT], marker='o', color=color, label=f"Q{component}")
+
+        # Adding labels and title
+        plt.xlabel('Q [kW]')
+        plt.ylabel('Temperature [C]')
+        plt.title('Streams To Cool')
+        plt.legend()
+        plt.grid(True)
+        if show:
+            plt.show()
+    
+    def plot_hexchange(self, show=False): #TODO: Move temperatures to the CHP or the MEA class!
+        Qsupp, Tsupp = self.QTdict["supp"]
+        Qlow, Tlow = self.QTdict["low"]
+        Qpinch, Tpinch = self.QTdict["pinch"]
+
         plt.figure(figsize=(10, 8))
         composite_curve = self.composite_curve
-        shifted_curve = [[point[0], point[1] - dTmin] for point in composite_curve]
+        shifted_curve = [[point[0], point[1] - self.dTmin] for point in composite_curve]
         (Qpinch-Qsupp) + (Qlow-Qpinch)
 
-
-        # Plot the elbow point on the curve
         plt.plot([0, self.get("Qreb")], [self.get("Treb"), self.get("Treb")], marker='*', color='#a100ba', label='Qreboiler')
         plt.plot([point[0] for point in composite_curve], [point[1] for point in composite_curve], marker='o', color='red', label='T of CCS streams')
         plt.plot([point[0] for point in shifted_curve], [point[1] for point in shifted_curve], marker='o', color='pink', label='T shifted')
@@ -403,7 +341,7 @@ class MEA_plant:
         plt.plot([Qpinch, Qsupp], [Tpinch, Tsupp], marker='x', color='blue', label='Qhighgrade')
         plt.plot([Qlow, composite_curve[-1][0]], [20, 15], marker='o', color='#0000FF', label='Cooling water') # NOTE: hard-coded CW temps.
 
-        plt.text(26000, 55, f'dTmin={dTmin} C', color='black', fontsize=12, ha='center', va='center')
+        plt.text(26000, 55, f'dTmin={self.dTmin} C', color='black', fontsize=12, ha='center', va='center')
         plt.text(26000, 115, f'Qreb={round(self.get("Qreb")/1000)} MW', color='#a100ba', fontsize=12, ha='center', va='center')       
         plt.text(5000, 60, f'Qhighgrade={round((Qpinch-Qsupp)/1000)} MW', color='#0000FF', fontsize=12, ha='center', va='center')
         plt.text(5000, 40, f'Qlowgrade={round((Qlow-Qpinch)/1000)} MW', color='#069AF3', fontsize=12, ha='center', va='center')
@@ -416,40 +354,3 @@ class MEA_plant:
         if show:
             plt.show()
         return
-
-    def exchanger_areas(self, Qhighgrade, Qlowgrade, Qcw, U, dTmin, Tmax, Tsupp, Tlow, Tend):
-        Alow = Qlowgrade/(U*dTmin) #Constant, see composite curve
-
-        dT1 = Tmax - Tsupp
-        dT2 = dTmin
-        dTlm = (dT1-dT2)/math.log(dT1/dT2)
-        Ahigh = Qhighgrade/(U*dTlm)
-
-        dT1 = (Tlow+dTmin) - 20         # ASSUMING CW of 15C->20C
-        dT2 = Tend - 15
-        dTlm = (dT1-dT2)/math.log(dT1/dT2)
-        Acw = Qcw/(U*dTlm)
-        return Alow, Ahigh, Acw
-    
-def find_points_on_curve(curve, x_values):
-    """
-    Find the y-values on the curve corresponding to specific x-values.
-
-    Parameters:
-        curve (list): List of points representing the curve in the form [[x1, y1], [x2, y2], ...].
-        x_values (list): List of x-values for which to find the corresponding y-values.
-
-    Returns:
-        dict: A dictionary containing the x-values as keys and their corresponding y-values as values.
-    """
-    points = {}
-    for x_target in x_values:
-        # Find the two closest points on the curve to the specified x-value
-        closest_points = sorted(curve, key=lambda point: abs(point[0] - x_target))[:2]
-        # Perform linear interpolation to find the y-value corresponding to the x-value
-        x1, y1 = closest_points[0]
-        x2, y2 = closest_points[1]
-        y_value = y1 + (y2 - y1) * (x_target - x1) / (x2 - x1)
-        # Add the interpolated y-value to the dictionary
-        points[x_target] = y_value
-    return points
